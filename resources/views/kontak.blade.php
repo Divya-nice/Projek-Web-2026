35fb235e7281c6996ad67608c4d3bc54129a49d1
Divya <241230011@unmuhpnk.ac.id>
2026-04-26 22:12:14 +0700

    Menambahkan Halaman Profil dan Kontak

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>Halaman Kontak</h1>
+    <p><mark>Hubungi Saya!</mark></p>
+    <p>Email : 241230011@unmuhpnk.ac.id</p>
+    <p>No.Hp : 081255028527</p>
+</div>
---
+++ resources/views/profil.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h1>Halaman Profil</h1>
+    <p><mark>Berikut ini profil saya</mark></p>
+    <p>Nama  : Divya Harinda Verlita</p>
+    <p>NIM   : 241230011</p>
+    <p>Kelas : 06 A</p>
+    <p>Prodi : Sistem Informasi</p>
+</div>
